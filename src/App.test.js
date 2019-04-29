46580276a9668b27781dd4c8a7d3cd6fa2698a24
Emrullah <emrullahayin@gmail.com>
2019-04-29 21:36:46 +0300

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Router from "./components/Router";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Router />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
