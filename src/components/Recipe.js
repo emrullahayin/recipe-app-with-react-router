92d76df00208ac14331364f989064a4581e1695f
Emrullah <emrullahayin@gmail.com>
2019-04-30 23:22:10 +0300

    getRecipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,6 +2,23 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Recipe = props => {
+  const [activeRecipe, setActiveRecipe] = useState();
+
+  const API_ID = "1d018658";
+  const API_KEY = "2d32770f279b4e12e5dc99d98b50573a";
+
+  const getRecipe = async () => {
+    const API_URL = `https://api.edamam.com/search?q=""&app_id=${API_ID}&app_key=${API_KEY}&to=50`;
+    try {
+      const response = await axios.get(API_URL);
+      //setRecipes(response.data.hits);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => console.log("getRecipe", getRecipe), []);
+  
   return (
     <div>
       <h1>React Components!</h1>
