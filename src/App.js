442a40eea7de0b5a1fb8076e0b348394c6fe1532
Emrullah <emrullahayin@gmail.com>
2019-05-01 02:03:36 +0300

    home page loading

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import Container from "react-bootstrap/Container";
+import Spinner from "react-bootstrap/Spinner";
 
 import SearchForm from "./components/SearchForm";
 import Recipes from "./components/Recipes";
@@ -13,6 +14,7 @@ const containerStyle = {
 const App = () => {
   const prevRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
   const [recipes, setRecipes] = useState(prevRecipes);
+  const [loading, setLoading] = useState(true);
 
   const API_ID = "1d018658";
   const API_KEY = "2d32770f279b4e12e5dc99d98b50573a";
@@ -23,8 +25,10 @@ const App = () => {
       try {
         const response = await axios.get(API_URL);
         setRecipes(response.data.hits);
+        setLoading(false);
       } catch (error) {
         console.error(error);
+        setLoading(false);
       }
     } else {
       alert("Please enter a recipe name");
@@ -34,17 +38,23 @@ const App = () => {
   const getRecipe = e => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
+    setLoading(true);
     fetchRecipe(recipeName);
   };
 
   useEffect(() => {
     localStorage.setItem("recipes", JSON.stringify(recipes));
+    localStorage.getItem("recipes").length && setLoading(false);
   }, [recipes]);
 
   return (
     <Container style={containerStyle}>
       <SearchForm getRecipe={getRecipe} />
-      <Recipes recipes={recipes} />
+      {loading === true ? (
+        <Spinner className="mt-3" animation="border" variant="primary" />
+      ) : (
+        <Recipes recipes={recipes} />
+      )}
     </Container>
   );
 };
